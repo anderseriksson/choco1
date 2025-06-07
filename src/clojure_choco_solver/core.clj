23f9e96beb53c978ebeb724aa19ea544038c3cd0
Anders Eriksson <anders.eriksson@avanza.se>
2025-06-07 23:03:01 +0200

    adding first contstraints

--- src/clojure_choco_solver/core.clj
+++ src/clojure_choco_solver/core.clj
@@ -1,8 +1,100 @@
 (ns clojure-choco-solver.core
   (:import [org.chocosolver.solver Model]
-           [org.chocosolver.solver.constraints Constraint]))
+           [org.chocosolver.solver.constraints Constraint] 
+           [org.chocosolver.solver.variables IntVar]))
+
+
+
+(defn assign-ids
+  "Takes a sequential collection and returns a map of {id value} with ids from 0 to n.
+   Throws an exception if input items are not unique."
+  [coll] 
+
+  (let [v (vec coll)]
+    (when (not= (count v) (count (set v)))
+      (throw (ex-info "All items in the collection must be unique." {:input coll})))
+    (into {} (map-indexed (fn [idx val] [idx val]) v))))
+
+(defn column-from-java-array
+  "Given a 2D Java array arr and index n, returns a vector of the nth element from each row."
+  [arr n]
+  (mapv #(aget ^objects arr % n) (range (alength arr))))
+
+
+
+(def ledare #{"Anders" "Tobias" "Jonas N" "Anna" "Jonas S"})
+
+(def ledare-nr (assign-ids ledare))
+
+
+
+
+
+(def patruller #{"Utmanarna" "Äventyrarna" "Upptäckarna 1" "Upptäckarna 2"})
+
+(def kalenderpass '("Söndag eftermiddag" "Söndag kväll" "Måndag förmiddag") )
+
+(def kalenderpass-nr
+  (assign-ids kalenderpass))
+
+(for [k kalenderpass
+      p patruller]
+  [k " " p])
+
+(def ledar-aktivitet #{"Förbereda landhajk" })
+(def aktivitet #{"Segla 2-krona 1" "Segla 2-krona 2" "Kanot" "Landhajk"})
+
+
+(defn print-patrullkalender
+  "Prints the values of the patrullkalenderaktivitet matrix."
+  [patrullkalenderaktivitet]
+  (doseq [i (range (alength patrullkalenderaktivitet))]
+    (println
+     (mapv (fn [j]
+             (.getValue ^org.chocosolver.solver.variables.IntVar (aget (aget patrullkalenderaktivitet i) j)))
+           (range (alength (aget patrullkalenderaktivitet i)))))))
+; Print the values of the patrullkalenderaktivitet matrix
+
+(let [model (Model. "Schema 0")
+      patrullkalenderaktivitet
+      (.intVarMatrix model "patrullkalender"
+                     (count kalenderpass)
+                     (count patruller)
+                     0 (count aktivitet))
+      c0 (.allDifferent model (into-array IntVar (aget patrullkalenderaktivitet 0)))
+      c1 (.allDifferent model (into-array IntVar (aget patrullkalenderaktivitet 1)))
+      c2 (.allDifferent model (into-array IntVar (aget patrullkalenderaktivitet 2)))
+      c3 (.allDifferent model (into-array IntVar  (column-from-java-array patrullkalenderaktivitet 0)))
+      c3b (.intValuePrecedeChain model (into-array IntVar  (column-from-java-array patrullkalenderaktivitet 0)) (into-array Integer/TYPE [0 1]))
+      c4 (.allDifferent model (into-array IntVar  (column-from-java-array patrullkalenderaktivitet 1)))
+      c4b (.intValuePrecedeChain model (into-array IntVar  (column-from-java-array patrullkalenderaktivitet 1)) (into-array Integer/TYPE [0 1]))
+      c5 (.allDifferent model (into-array IntVar  (column-from-java-array patrullkalenderaktivitet 2)))
+      c5b (.intValuePrecedeChain model (into-array IntVar  (column-from-java-array patrullkalenderaktivitet 2)) (into-array Integer/TYPE [0 1]))
+      c6 (.allDifferent model (into-array IntVar  (column-from-java-array patrullkalenderaktivitet 3)))
+      c6b (.intValuePrecedeChain model (into-array IntVar  (column-from-java-array patrullkalenderaktivitet 3)) (into-array Integer/TYPE [0 1]))
+      
+      
+      ]
+  (.post model (into-array Constraint [c0 c1 c2 c3 c3b c4 c4b c5 c5b  c6 c6b]))
+  (println "constraints:" (.getCstrs model))
+  (let [solver (.getSolver model)]
+    (loop [i 0]
+      (when (and (< i 100) (.solve solver))
+        (println "patrullkalender =" )
+        (print-patrullkalender patrullkalenderaktivitet)
+        (recur (inc i))))
+    (println "No more solutions or limit reached.")))
+
+
+
 
 (defn -main
+  "Main function to run the Choco solver example."
+  [& args]
+  (println "Running Choco solver example...")
+  ())
+
+(defn -mainx
   "Demo: Solve x + y = 5 with x, y in 0..5"
   [& args]
   (let [model (Model. "Simple Addition")
@@ -15,6 +107,9 @@
       (while (.solve solver)
         (println "x =" (.getValue x) ", y =" (.getValue y))))))
 
+
 ;; To run: clojure -M -m clojure-choco-solver.core
+
+;; https://javadoc.io/doc/org.choco-solver/choco-solver/4.10.18/org.chocosolver.solver/org/chocosolver/solver/Model.html
 
 
